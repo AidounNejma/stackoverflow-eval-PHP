4602dac338b33b08dd4b8a5df970721929483efb
AidounNejma <guermachenejma@gmail.com>
2022-03-12 11:43:45 +0100

    error login fixed to ask question

--- src/View/questions/allQuestions.php
+++ src/View/questions/allQuestions.php
@@ -15,37 +15,38 @@ require_once './src/View/includes/header.inc.php';
     <a href="?page=ask" class="btn btn-primary askQuestion">Ask Question</a>
 </div>
 <div class="line my-4"></div>
+
 <?php foreach ($questions as $question) : ?>
-    <?php if($question->getStatus() == "published") :?>
-    <div class="containerQuestion">
+    <?php if ($question->getStatus() == "published") : ?>
+        <div class="containerQuestion">
 
-        <div class="left">
-            <p><?= $question->created_at ?></p>
-        </div>
+            <div class="left">
+                <p><?= $question->created_at ?></p>
+            </div>
 
-        <div class="right">
-            <a href="?page=question&id=<?= $question->getId() ?>"><?= $question->getTitle() ?></a>
+            <div class="right">
+                <a href="?page=question&id=<?= $question->getId() ?>"><?= $question->getTitle() ?></a>
 
-            <div class="infoFlex">
-                <a href="?page=index&tag=<?= $question->getTechnology() ?>" class="btn btn-primary tag"><?= $question->getTechnology() ?></a>
-                <?php foreach ($users as $user) : ?>
-                    <?php if ($question->user_id == $user->getId()) : ?>
-                        <a href="#"><?= $user->getNickname() ?></a>
-                    <?php endif ?>
-                <?php endforeach ?>
+                <div class="infoFlex">
+                    <a href="?page=index&tag=<?= $question->getTechnology() ?>" class="btn btn-primary tag"><?= $question->getTechnology() ?></a>
+                    <?php foreach ($users as $user) : ?>
+                        <?php if ($question->user_id == $user->getId()) : ?>
+                            <a href="#"><?= $user->getNickname() ?></a>
+                        <?php endif ?>
+                    <?php endforeach ?>
+                </div>
+
             </div>
 
         </div>
-
-    </div>
-    <div class="line"></div>
-<?php endif ?>
+        <div class="line"></div>
+    <?php endif ?>
 <?php endforeach ?>
 
 <nav aria-label="Page navigation example" class="d-flex justify-content-center">
     <ul class="pagination">
-        <li class="page-item"><a class="page-link" href="?page=index&p=<?= $page-1 ?>&search=<?= $search?>">Previous</a></li>
-        <li class="page-item"><a class="page-link" href="?page=index&p=<?= $page+1 ?>&search=<?= $search?>">Next</a></li>
+        <li class="page-item"><a class="page-link" href="?page=index&p=<?= $page - 1 ?>&search=<?= $search ?>">Previous</a></li>
+        <li class="page-item"><a class="page-link" href="?page=index&p=<?= $page + 1 ?>&search=<?= $search ?>">Next</a></li>
     </ul>
 </nav>
 
--- src/View/questions/askQuestion.php
+++ src/View/questions/askQuestion.php
@@ -9,39 +9,41 @@ require_once './src/View/includes/header.inc.php';
     <p class="px-2"> > </p>
     <p>Ask A question</p>
 </div>
+<?php if (isset($_GET['id'])) : ?>
+    <form action="" method="post">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
 
-<form action="" method="post">
-    <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                    <div class="card-body p-5 text-center">
+                            <h3 class="mb-5">Want to ask a question ?</h3>
 
-                        <h3 class="mb-5">Want to ask a question ?</h3>
+                            <div class="form-outline mb-4">
+                                <input type="text" id="title" name="title" class="form-control form-control-lg" required />
+                                <label class="form-label" for="title">Title</label>
+                            </div>
 
-                        <div class="form-outline mb-4">
-                            <input type="text" id="title" name="title" class="form-control form-control-lg" required/>
-                            <label class="form-label" for="title">Title</label>
+                            <div class="form-outline mb-4">
+                                <input type="text" id="technology" name="technology" class="form-control form-control-lg" required />
+                                <label class="form-label" for="technology">Technology</label>
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <textarea name="content" id="content" class="form-control form-control-lg" required> </textarea>
+                                <label class="form-label" for="content">Content</label>
+                            </div>
+
+                            <button class="btn btn-primary btn-lg btn-block" id="submitAskQuestion" type="submit">Submit</button>
                         </div>
-
-                        <div class="form-outline mb-4">
-                            <input type="text" id="technology" name="technology" class="form-control form-control-lg" required/>
-                            <label class="form-label" for="technology">Technology</label>
-                        </div>
-
-                        <div class="form-outline mb-4">
-                            <textarea name="content" id="content" class="form-control form-control-lg" required> </textarea>
-                            <label class="form-label" for="content">Content</label>
-                        </div>
-
-                        <button class="btn btn-primary btn-lg btn-block" id="submitAskQuestion" type="submit">Submit</button>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</form>
-
+    </form>
+<?php else : ?>
+    <h5 class="py-4 text-center notificationLoginIn">To ask a question you have to be logged in. You can log in <a href="?page=login">here.</a></h5>
+<?php endif ?>
 
 
 <?php
